5e31649541952d7f9c94dcb475de069d53fd3f38
Judith Cobbinah <judith_cobbinah@cable.comcast.com>
2021-06-21 03:11:33 -0400

    work in progress 3

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ window.addEventListener("load", function(){
       let fuelInput = document.getElementById("fuelValue").value;
       let cargoInput = document.getElementById("cargoValue").value;
 
+      if ((pilotInput === '')||  (coPilotInput === ' ')|| (fuelInput === '')|| (cargoInput === '')) {
+         alert("All information must be entered before takeoff can commence");
+         event.preventDefault();
+      } else if (isNaN(fuelInput) === true || isNaN(cargoInput) === true) {
+         alert("Fuel Level and Cargo mass must be numbers. Fuel minimum is 10,000 L, and Cargo maximum is 10,000 kilograms");
+         
+      }
+
    })
 })
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
