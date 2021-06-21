4305742b8acccdcfb19d26883ac8fb7d3591d375
Judith Cobbinah <judith_cobbinah@cable.comcast.com>
2021-06-21 02:17:54 -0400

    work in progress 1

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotInput = document.getElementById("pilotName").value;
+      let coPilotInput = document.getElementById("coPilotName").value;
+      let fuelInput = document.getElementById("fuelValue").value;
+      let cargoInput = document.getElementById("cargoValue").value;
+
+   })
+})
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -11,3 +21,5 @@
 </ol>
 <img src="${}">
 */
+
+Function
